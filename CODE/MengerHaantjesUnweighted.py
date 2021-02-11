2d2d675fcb239609bc53c6714663778714d0d0a6
asamallab <75881572+asamallab@users.noreply.github.com>
2021-02-11 07:06:47 +0530

    Update MengerHaantjesUnweighted.py

--- CODE/MengerHaantjesUnweighted.py
+++ CODE/MengerHaantjesUnweighted.py
@@ -4,7 +4,7 @@ Program to compute Menger and Haantjes curvature for edges in unweighted and
 undirected network.
 Written by: Areejit Samal
 Reference:
-(1) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, (Submitted).
+(1) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R. Soc. Open Sci. 8: 201734 (2021).
 (2) E. Saucan, A. Samal & J. Jost. A simple differential geometry for complex networks. Network Science, 1-28 (2020). doi:10.1017/nws.2020.42.
 (3) E. Saucan, A. Samal & J. Jost, A Simple Differential Geometry for Networks and Its Generalizations In: H. Cherifi, S. Gaito, J. Mendes, E. Moro, L. Rocha (Eds) Complex Networks and Their Applications VIII. COMPLEX NETWORKS 2019. Studies in Computational Intelligence, Vol. 881. Springer. 
 
