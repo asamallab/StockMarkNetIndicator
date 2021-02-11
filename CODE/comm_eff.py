d23b79e320b2cdf6a226c2444f787c5e98a7f09e
asamallab <75881572+asamallab@users.noreply.github.com>
2021-02-11 07:07:22 +0530

    Update comm_eff.py

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -8,7 +8,7 @@
 =================================================================================================
 If you are using this code, kindly cite the following articles:
 1) S. Venkatesan, R.P. Vivek-Ananth, R.P. Sreejith, P. Mangalapandi, A.A. Hassanali & A. Samal, Network approach towards understanding the crazing in glassy amorphous polymers, Journal of Statistical Mechanics: Theory and Experiment 043305 (2018).
-2) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, (Submitted).
+2) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R. Soc. Open Sci. 8: 201734 (2021).
 =================================================================================================
 """
 
