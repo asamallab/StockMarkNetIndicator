b7722fd4ee1c4155d2858eea6a5cd8c1ed14b3ea
asamallab <75881572+asamallab@users.noreply.github.com>
2021-02-16 14:50:13 +0530

    Update analysis_matlab.m

--- CODE/analysis_matlab.m
+++ CODE/analysis_matlab.m
@@ -8,7 +8,6 @@
 %% (4) minimum risk Markowitz portfolio
 %% If you are using this code, kindly cite the following article:
 %% A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R. Soc. Open Sci. 8: 201734 (2021).
-%% Network geometry and market instability, (Submitted).
 %% To work with moving epochs, we denote Fr: epoch #; Pr: price time series as input; daily_retn= daily logarithmic returns matrix of stock prices
 %%
 %% function for index returns
